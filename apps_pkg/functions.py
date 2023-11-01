cad4df63a421bccdce93734e18ee0b38b169e07b
Oluchukwu Nwade <oluchinwade@gmail.com>
2023-11-01 14:07:09 +0000

    Functions of the game

---
+++ apps_pkg/functions.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def change_username(self, name):
+        self.name = name 
+        print(f"Your new username has been changed to {self.name}")
+
+class Game:
+    def __init__(self):
+        pass 
+
+    def get_choices(self, car, colour, profession,  ):
+        pass
+
+    def RandomNumberGeration():
+        pass
